2203f44f7196045577731be094c053dac69d3afd
Bruno Martins <bgcngm@gmail.com>
2017-12-10 23:23:40 +0000

    klteactivexx: init: Update extension for O-mr1
    Change-Id: I2fcd62b1dd6cbe292d89cf97085c9549bf69ba14

--- init/init_klte.cpp
+++ init/init_klte.cpp
@@ -30,15 +30,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <android-base/logging.h>
 #include <android-base/properties.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
-#include "log.h"
 
 #include "init_msm8974.h"
 
 using android::base::GetProperty;
+using android::init::property_set;
 
 void gsm_properties()
 {
@@ -64,6 +65,6 @@ void init_target_properties()
     }
 
     std::string device = GetProperty("ro.product.device", "");
-    LOG(INFO) << "Found bootloader id " << bootloader <<  " setting build properties for "
-	    << device <<  " device" << std::endl;
+    LOG(ERROR) << "Found bootloader id " << bootloader <<  " setting build properties for "
+        << device <<  " device" << std::endl;
 }
