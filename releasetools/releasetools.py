0142b44c489bd3ea52bf01c73bc09cb318b62b4c
Nolen Johnson <johnsonnolen@gmail.com>
2020-04-12 23:12:43 -0400

    klteactivexx: Add klteattactive support
    * DON'T USE THIS, UNDER ANY CIRCUMSTANCES!
    
    Change-Id: Iddeb11f4c80cd40c382aad963c4283247d2ce349

---
+++ releasetools/releasetools.py
@@ -0,0 +1,9 @@
+"""Custom OTA commands for klte* devices with locked bootloaders"""
+
+def FullOTA_InstallEnd(info):
+  info.script.script = [cmd for cmd in info.script.script if not "boot.img" in cmd]
+  info.script.script = [cmd for cmd in info.script.script if not "show_progress(0.100000, 0);" in cmd]
+  info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/bootdevice/by-name/system", "/mnt/system");');
+  info.script.AppendExtra('package_extract_file("boot.img", "/tmp/boot.img");')
+  info.script.AppendExtra('assert(run_program("/sbin/sh", "/system/etc/dtc.sh") == 0);')
+  info.script.AppendExtra('unmount("/mnt/system");');
